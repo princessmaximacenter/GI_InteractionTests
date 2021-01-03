75cb84eedc395b9b36297065fa558650c6cd95cd
Josephine Daub <josephinedaub@hotmail.com>
2021-01-03 16:36:07 +0100

    Improved cleaning up of PAN conf directories

--- wesme/copy_conf_PAN_files.sh
+++ wesme/copy_conf_PAN_files.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-#input arguments: project, nr of repeats (10)
+#input arguments: project, nr of repeats (usually 9)
 project=$1
 start_repeats=$2
 end_repeats=$3
@@ -17,7 +17,12 @@ do
   mv -f ./${project}_conf_${i}/results/fdr/PAN_smut_co_1.1_300_fdr_2bins_2.txt \
   ./${project}_conf/results/fdr/PAN_${i}_smut_co_1.1_300_fdr_2bins_2.txt
 
-  rm -r ./${project}_conf_${i}/preproc/permuted_pv/PAN
+  mv -f ./${project}_conf_${i}/results/PAN_smut_me_pvs_1.1.txt \
+  ./${project}_conf/results//PAN_${i}_smut_me_pvs_1.1.txt
+  mv -f ./${project}_conf_${i}/results/PAN_smut_co_pvs_1.1.txt \
+  ./${project}_conf/results//PAN_${i}_smut_co_pvs_1.1.txt
+
+  #rm -r ./${project}_conf_${i}/preproc/permuted_pv/PAN
   # remove <project>_conf_xx directories
-  #rm -r ./${project}_conf_${i}
+  rm -r ./${project}_conf_${i}
 done
